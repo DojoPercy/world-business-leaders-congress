eacb96a404b5b4591ffdd752ff69a92388e47476
David ojo <97886267+DojoPercy@users.noreply.github.com>
2025-12-23 03:05:01 +0100

    Update event date for WBLC 2025

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   shortName: 'WBLC 2025',
   description: 'Business Beyond Profit: Shaping a Sustainable and Equitable Future',
   event: {
-    date: '30th - 2nd march, 2026',
+    date: '30th march - 2nd April, 2026',
     location: 'Dubai, UAE',
     theme: 'Business Beyond Profit: Shaping a Sustainable and Equitable Future'
   },
